81932372bf9d9a2cb38eed96556e116e526f8eec
Eirik S. Morland <eirik@morland.no>
2020-01-26 20:36:37 +0100

    Avoid removing blockquote style. Create a sitemap. Fix #22. Fix #18

--- gatsby-config.js
+++ gatsby-config.js
@@ -48,11 +48,12 @@ module.exports = {
         printRejected: true, // Print removed selectors and processed file names
         // develop: true, // Enable while using `gatsby develop`
         tailwind: true, // Enable tailwindcss support
-        whitelist: ['#gatsby-noscript'], // Don't remove this selector
+        whitelist: ['#gatsby-noscript', 'blockquote'], // Don't remove this selector
         ignore: ['/ignored.css', 'prismjs/', 'docsearch.js/'], // Ignore files/folders
         // purgeOnly : ['components/', '/main.css', 'bootstrap/'], // Purge only these files/folders
       }
     },
+    `gatsby-plugin-sitemap`,
     {
       resolve: `gatsby-plugin-feed`,
       options: {
