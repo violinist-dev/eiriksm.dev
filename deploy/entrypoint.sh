99f19603854d7b8319300c209cfbb2a87ef7f173
Eirik Stanghelle Morland <eirik@morland.no>
2023-12-12 12:20:38 +0100

    Test the deploy as well (#121)

--- deploy/entrypoint.sh
+++ deploy/entrypoint.sh
@@ -6,5 +6,9 @@ wget $DISQUS_XML_URL -O disqus.xml
 echo "unsafe-perm = true" > .npmrc
 npm i
 npm run build
-npm run deploy
-aws cloudfront create-invalidation --distribution-id $DIST_ID --paths '/*'
+if [ "$DEPLOY" == "0" ]; then
+    echo "No deploy since DEPLOY is not set to true"
+else
+    npm run deploy
+    aws cloudfront create-invalidation --distribution-id $DIST_ID --paths '/*'
+fi
